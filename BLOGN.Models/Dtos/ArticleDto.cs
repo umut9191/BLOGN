162694bb214a2d52279c554f1a26be8663a521b8
umut9191 <umutsurmeli91@gmail.com>
2022-04-17 20:46:16 +0300

    Article Model and Dto added

---
+++ BLOGN.Models/Dtos/ArticleDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLOGN.Models.Dtos
+{
+    public class ArticleDto
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Content { get; set; }
+        public DateTime ArticleDate { get; set; }
+        [Required]
+        public int CategoryId { get; set; }
+     
+        public Category Category { get; set; }
+    }
+}
