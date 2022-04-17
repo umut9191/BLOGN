00250a2848ab1fdd1a515f76238aadea78500d4d
umut9191 <umutsurmeli91@gmail.com>
2022-04-17 23:35:35 +0300

    if user has correct userName and Password then UserRepository -> Authenticate  method creates new token for that user

--- BLOGN.Data/Repositories/Repository/UserRepository.cs
+++ BLOGN.Data/Repositories/Repository/UserRepository.cs
@@ -2,9 +2,12 @@
 using BLOGN.Models;
 using BLOGN.SharedTools;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,7 +17,7 @@ namespace BLOGN.Data.Repositories.Repository
     {
         private ApplicationDbContext _context;
         private readonly AppSettings _appSettings;
-        public UserRepository(ApplicationDbContext context,IOptions<AppSettings> appSettings) : base(context)
+        public UserRepository(ApplicationDbContext context, IOptions<AppSettings> appSettings) : base(context)
         {
             _context = context;
             _appSettings = appSettings.Value;
@@ -22,7 +25,29 @@ namespace BLOGN.Data.Repositories.Repository
 
         public User Authenticate(string userName, string password)
         {
-            throw new NotImplementedException();
+            var user = _context.Users.SingleOrDefault(u => u.UserName == userName && u.Password == password);
+            if (user == null)
+            {
+                return null;
+            }
+            //if it is not null then create new token;
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name,user.Id.ToString()),
+                    new Claim(ClaimTypes.Role,user.Role),
+                }),
+                Expires = DateTime.UtcNow.AddDays(5),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            user.Token = tokenHandler.WriteToken(token);
+            user.Password = "";
+            return user;
         }
 
         public bool IsUniqueUser(string userName)
