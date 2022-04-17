37cfe29dc728e8ebf00303e4f8c29c32f54c9eec
umut9191 <umutsurmeli91@gmail.com>
2022-04-17 20:37:23 +0300

    Category endpoints codes added and missing parts complated

--- BLOGN.API/Controllers/CategoryController.cs
+++ BLOGN.API/Controllers/CategoryController.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
 using BLOGN.Data.Services.IServices;
+using BLOGN.Models;
+using BLOGN.Models.Dtos;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,6 +17,46 @@ namespace BLOGN.API.Controllers
         {
             _categoryServices = categoryServices;
             _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var categories = await _categoryServices.GetAll();
+            var categoryDto = _mapper.Map<IEnumerable<CategoryDto>>(categories);
+            return Ok(categoryDto);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var category = await _categoryServices.Get(id);
+            var categoryDto = _mapper.Map<CategoryDto>(category);
+            return Ok(categoryDto);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Save(CategoryDto categoryDto)
+        {
+            var category = _mapper.Map<Category>(categoryDto);
+            var newCategory = await _categoryServices.Add(category);
+            return Created(string.Empty,null);
+        }
+        [HttpPatch]
+        public IActionResult Patch(int id, CategoryDto categoryDto)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }  
+            var category = _mapper.Map<Category>(categoryDto);
+            _categoryServices.Update(category);
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+           await _categoryServices.Delete(id);
+            return NoContent();
+
         }
     }
 }
