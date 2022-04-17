e4a0ed941ebb96470a38418504808e90055362b8
umut9191 <umutsurmeli91@gmail.com>
2022-04-17 14:51:52 +0300

    UnitOfWork codes added

---
+++ BLOGN.Data/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using BLOGN.Data.Repositories.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLOGN.Data.UnitOfWork
+{
+    public interface IUnitOfWork:IDisposable
+    {
+        ICategoryRepository Category { get; }
+        Task SaveAsync();
+        void Save();
+    }
+}
---
+++ BLOGN.Data/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using BLOGN.Data.Repositories.IRepository;
+using BLOGN.Data.Repositories.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLOGN.Data.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public ICategoryRepository Category => new CategoryRepository(_context);
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task SaveAsync()
+        {
+          await  _context.SaveChangesAsync();
+        }
+    }
+}
