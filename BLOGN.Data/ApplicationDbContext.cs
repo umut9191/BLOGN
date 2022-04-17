ff422a03b23c5a3f0d13f5ce71e51517bc563cff
umut9191 <umutsurmeli91@gmail.com>
2022-04-17 12:17:40 +0300

    Data,Models Layers,Constext codes, repository pattern codes added

---
+++ BLOGN.Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using BLOGN.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLOGN.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
