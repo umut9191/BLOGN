40379fb5f3c1c95b0286affe5a703523613b5eeb
umut9191 <umutsurmeli91@gmail.com>
2022-04-17 22:36:23 +0300

    User repository codes added

---
+++ BLOGN.Data/Repositories/Repository/UserRepository.cs
@@ -0,0 +1,19 @@
+﻿using BLOGN.Data.Repositories.IRepository;
+using BLOGN.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLOGN.Data.Repositories.Repository
+{
+    public class UserRepository : Repository<User>, IUserRepository
+    {
+        private ApplicationDbContext _context;
+        public UserRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context
+        }
+    }
+}
