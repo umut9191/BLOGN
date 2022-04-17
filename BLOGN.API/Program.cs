daeefbbf6afa28bd5d297101db15b88836580900
umut9191 <umutsurmeli91@gmail.com>
2022-04-17 23:09:21 +0300

    Cors configuration added inside program.cs file

--- BLOGN.API/Program.cs
+++ BLOGN.API/Program.cs
@@ -62,6 +62,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 app.UseAuthentication();
 app.UseAuthorization();
 
