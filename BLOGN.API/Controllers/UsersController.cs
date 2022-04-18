d84223979c0392eadab2cfcfb9b00ff45ca886bc
umut9191 <umutsurmeli91@gmail.com>
2022-04-18 22:19:08 +0300

    UsersController class created

---
+++ BLOGN.API/Controllers/UsersController.cs
@@ -0,0 +1,17 @@
+﻿using BLOGN.Data.Repositories.IRepository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BLOGN.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+        public UsersController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+    }
+}
